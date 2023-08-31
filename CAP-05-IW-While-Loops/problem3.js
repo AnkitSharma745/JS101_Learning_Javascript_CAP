b0273b439e8ee536376862c0f1dbb5b82cddb860
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-08-31 14:24:05 +0530

    done

---
+++ CAP-05-IW-While-Loops/problem3.js
@@ -0,0 +1,11 @@
+// Problem 3: Print the sum of all the multiples of 3 from 0 to the given limit
+
+let sum =0
+let limit =19
+let start =0
+while(start<=limit){
+  let mul = start*3
+  sum+=mul
+start++
+}
+console.log(sum)
---
+++ CAP-05-IW-While-Loops/problem4.js
@@ -0,0 +1,11 @@
+// Problem 4: Print the average of even numbers from 1 to 100 (both included)
+
+
+let start = 1
+let end = 100
+while(start<=end){
+  if(start%2==0){
+    console.log(start)
+  }
+  start++
+}
